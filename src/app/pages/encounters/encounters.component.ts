f1acef3ab1f7055e9f1eca2b9c8d5ae2fe645470
Brian Lau <lauwangtatbrian@gmail.com>
2017-05-30 14:53:01 -0700

    added ajax request for encounters

--- src/app/pages/encounters/encounters.component.ts
+++ src/app/pages/encounters/encounters.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Encounter } from '../../models/encounter';
+import { EncountersService } from '../../services/encounters.service';
 
 @Component({
   selector: 'app-encounters',
   templateUrl: './encounters.component.html',
-  styleUrls: ['./encounters.component.scss']
+  styleUrls: ['./encounters.component.scss'],
+  providers: [EncountersService]
 })
 export class EncountersComponent implements OnInit {
 
-  constructor() { }
+  encounters: Encounter[] = [];
+
+  constructor(private encounterService: EncountersService) {}
 
   ngOnInit() {
+    this.encounterService.getData()
+        .subscribe((data) => {
+          console.log(data);
+          this.encounters = data.encounters;
+        });
   }
-
 }
---
+++ src/app/services/encounters.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Encounter } from '../models/encounter';
+
+@Injectable()
+export class EncountersService {
+  private ENCOUNTERS_URL = 'https://red-wdp-api.herokuapp.com/api/mars/encounters';
+
+  constructor(private http: Http) { }
+
+  getData() {
+    return this.http.get(this.ENCOUNTERS_URL)
+                    .map(this.extractEncounters);
+  }
+
+  extractEncounters(res: Response) {
+    const encounter = res.json();
+    return encounter;
+  }
+
+  handleError() {
+    console.log('There was an error');
+  }
+
+}
